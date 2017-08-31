fd22641d3a4fbc8339e1e9bcf67a4c39c182b51b
App Academy Student <student@appacademy.io>
2017-08-31 13:40:57 -0700

    Add transpose method

--- phase_1_arrays.js
+++ phase_1_arrays.js
@@ -17,11 +17,25 @@ Array.prototype.twoSum = function () {
     for (var j = i + 1; j < this.length; j++) {
       if (i === j) {
         continue;
-      }
-      else (this[i] + this[j] === 0) {
+      } else if (this[i] + this[j] === 0) {
         sumArr.push([i, j]);
       }
     }
   }
   return sumArr;
+};
+
+Array.prototype.transpose = function () {
+  const transposeArr = [];
+  for (var i = 0; i < this.length; i++) {
+    for (var j = 0; j < this[i].length; j++) {
+      if (i === 0) {
+        transposeArr.push([this[i][j]]);
+      }
+      else {
+        transposeArr[j].push(this[i][j]);
+      }
+    }
+  }
+  return transposeArr;
 };
