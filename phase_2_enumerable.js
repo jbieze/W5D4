80121256ef8720b49a9c00b06e7424bd1022d804
App Academy Student <student@appacademy.io>
2017-08-31 15:07:54 -0700

    Finish Phase 2

--- phase_2_enumerable.js
+++ phase_2_enumerable.js
@@ -1,6 +1,42 @@
+function multByTwo(num) {
+  return num * 2;
+}
+
 Array.prototype.myEach = function (callback) {
   for (var i = 0; i < this.length; i++) {
     callback(this[i]);
   }
-  return undefined;
 };
+
+Array.prototype.myMap = function (callback) {
+  const returnArr = [];
+  this.myEach(el => {
+    returnArr.push(callback(el));
+  });
+  return returnArr;
+};
+
+Array.prototype.myReduce = function (callback, accumulator) {
+  const arr = this.slice(0);
+
+  if (accumulator === undefined) {
+    accumulator = arr.shift();
+  }
+
+  arr.myEach( (el) => {
+    accumulator = callback(accumulator, el);
+  });
+
+  return accumulator;
+};
+
+
+// // without initialValue
+// [1, 2, 3].myReduce(function(acc, el) {
+//   return acc + el;
+// }); // => 6
+//
+// // with initialValue
+// [1, 2, 3].myReduce(function(acc, el) {
+//   return acc + el;
+// }, 25); // => 31
