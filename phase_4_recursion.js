ceccda6ec1840d42362a0ff0bd5bf9da7432e0be
App Academy Student <student@appacademy.io>
2017-08-31 17:17:13 -0700

    Write #exp versions 1 and 2

--- phase_4_recursion.js
+++ phase_4_recursion.js
@@ -15,13 +15,45 @@ function sumRec(arr) {
     let lastEl = arr[arr.length - 1];
     let remainingArr = arr.slice(0, -1);
     return sumRec(remainingArr) + lastEl;
-
   }
 }
 
-function exponent(base, exp) {
-
+function exponent1(base, exp) {
+  if (exp === 0) {
+    return 1;
+  } else {
+    return base * exponent1(base, exp - 1);
+  }
 }
+
+function exponent2(base, exp) {
+  if (exp === 0) {
+    return 1;
+  } else if (exp === 1) {
+    return base;
+  } else {
+    if (exp % 2 === 0) {
+      return Math.pow(exponent2(base, Math.floor(exp / 2)), 2);
+    } else {
+      return base * Math.pow(exponent2(base, Math.floor(exp / 2)), 2);
+    }
+  }
+}
+
+// function exponent2(base, exp) {
+//   if (exp === 0) {
+//     return 1;
+//   } else if (exp === 1) {
+//     return base;
+//   } else {
+//     if (exp % 2 === 0) {
+//       let
+//       return exponent2(base, exp / 2) * exponent2(base, exp / 2);
+//     } else {
+//       return base * exponent2(base, exp / 2) * exponent2(base, exp / 2);
+//     }
+//   }
+// }
 
 function fibonacci(n) {
 
